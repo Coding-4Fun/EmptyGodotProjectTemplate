038017f470cc479e166c2127cfa090aaf5bacc49
Dirk Buck <dirk.buck@wolsdorff-tobacco.de>
2022-04-12 12:45:06 +0200

    Add: Einfaches initialisieren eines Clients

--- game/networking/SimpleClient.gd
+++ game/networking/SimpleClient.gd
@@ -3,12 +3,21 @@ class_name SimpleClient
 extends Node
 
 # Connect all functions
+var clientpeer
+var const SERVER_IP = 127.0.0.1 setget setHostIP
+var const SERVER_PORT = 7777 setget setServerPort
 
 func _ready():
 	get_tree().connect("connected_to_server", self, "_connected_ok")
 	get_tree().connect("network_peer_connected", self, "_player_connected")
 	get_tree().connect("server_disconnected", self, "_server_disconnected")
 	get_tree().connect("connection_failed", self, "_connected_fail")
+
+
+func _initÖ_Client() -> void:
+	var clientpeer = NetworkedMultiplayerENet.new()
+	clientpeer.create_client(SERVER_IP, SERVER_PORT)
+	get_tree().network_peer = clientpeer
 
 
 # Only called on clients, not server. Will go unused; not useful here.
@@ -28,3 +37,10 @@ func _server_disconnected():
 # Could not even connect to server; abort.
 func _connected_fail():
 	pass 
+
+
+func setServerPort(port:int)
+	if port > 0 and port <= 65535
+		SERVER_PORT = port
+	# else
+		# Error, Port out of Range
