c4be35ed7c015ad67bf6b6e59a58fc99312683d6
Dirk Buck <dirk.buck@wolsdorff-tobacco.de>
2022-04-12 12:35:18 +0200

    Add: Einfaches initialisieren eines Servers Add: Setter für Server Port und Max Connections

--- game/networking/SimpleServer.gd
+++ game/networking/SimpleServer.gd
@@ -2,8 +2,19 @@ class_name SimpleServer
 
 extends Node
 
-# Connect all functions
+# TCP Calls. zuverlässig, geordnet und langsam
+# rpc("function_name", <optional_args>)
+# rpc_id(<peer_id>,"function_name", <optional_args>)
 
+# UDP Calls, unzuverlässig, ungeordnet und schnell
+# rpc_unreliable("function_name", <optional_args>)
+# rpc_unreliable_id(<peer_id>, "function_name", <optional_args>)
+
+var serverpeer	# NetworkedMultiplayerPeer-Objekt
+var const SERVER_PORT = 7777 setget setServerPort
+var const MAX_PLAYERS = 2 setget setMaxPlayer
+
+# Connect all functions
 func _ready():
 	get_tree().connect("network_peer_connected", self, "_player_connected")
 	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
@@ -16,3 +27,16 @@ func _player_disconnected(id):
 	pass
 
 
+func setMaxPlayer(player:int)
+	MAX_PLAYERS = player
+
+
+func setServerPort(port:int)
+	if port > 0 and port <= 65535
+	SERVER_PORT = port
+
+
+func _init_server() -> void: 
+	serverpeer = NetworkedMultiplayerENet.new()
+	serverpeer.create_server(SERVER_PORT, MAX_PLAYERS)
+	get_tree().network_peer = serverpeer
